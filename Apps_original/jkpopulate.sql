bd7404902900a842531be56c5fbed63052cdcf50
Joel Klein <jdk514@gwmail.gwu.edu>
2013-05-07 04:04:29 -0400

    fixed search for reviewers

--- Apps_original/jkpopulate.sql
+++ Apps_original/jkpopulate.sql
@@ -16,5 +16,41 @@ INSERT INTO `Recommendation` VALUES(142, 12693152, 'Jack Smith', 'Professor', 'j
 INSERT INTO `GA` VALUES(35631579, 'You shall not pass');
 
 
-INSERT INTO `Applications` VALUES(54582981, 'Learning', 15222586, 'MS', 'BS', 'ALL', 'Temple', '4.5', 'BS', '', '', '', '2013-04-17 16:29:05', 'I just study', 'Fall 2013', '', 'Notviewed', '2010-04-24', NULL, NULL);
+INSERT INTO `Applications` VALUES(54582981, 'Learning', 15222586, 'MS', 'BS', 'ALL', 'Temple', '4.0', 'BS', '', '', '', '2013-04-17 16:29:05', 'I just study', 'Fall 2013', '', 'Notviewed', '2010-04-24', NULL, NULL);
 INSERT INTO `Applications` VALUES(50568867, 'Fencing', 12693152, 'MS', 'BS', 'Comp Sci', 'GWU', '3.7', 'BS', '', '', '', '2013-04-17 16:22:09', 'TA at GWU', 'Fall 2013', '', 'viewed', '1992-06-04', 'Looks good, but can afford our institution and provides no other warrant for admitting with aid.', 'Admit without Aid');
+
+
+
+INSERT INTO `Transcript` VAlues(54582981, 15222586, 4.0, 135, 15, 3, 154, 15);
+INSERT INTO `Transcript` VAlues(50568867, 12693152, 3.7, 135, 15, 3, 154, 15);
+
+INSERT INTO `Student` VALUES(12222222, 'John', 'Lennon', 'jlennon@gwmail.gwu.edu', 'l8qv76fxJL', 'Waiting for Review');
+INSERT INTO `Applications` VALUES(489649863, 'Studying', 12222222, 'MS', 'BS', 'Journalism', 'Temple', '3.72', 'BS', '', '', '', '2013-04-17 16:29:05', 'Interned as IT', 'Fall 2013', '', 'Notviewed', '2010-04-24', NULL, NULL, '908-894-9364', '842-93-9486');
+INSERT INTO `Transcript` VAlues(489649863, 12222222, 3.72, 135, 15, 3, 154, 15);
+INSERT INTO `Recommendation` VALUES(160, 12222222, 'Mike Spleen', 'Professor', 'mspleen@this.not.gwu', '', 'He taught him for a while', NULL);
+INSERT INTO `Recommendation` VALUES(161, 12222222, 'Paul Lennon', 'Father', 'plennon@this.not.gwu', '', 'He raised him to be the man he is', NULL);
+INSERT INTO `Recommendation` VALUES(162, 12222222, 'Carol Evans', 'Manager', 'cevans@this.not.gwu', '', 'He was my boss when I worked as IT', NULL);
+
+INSERT INTO `Student` VALUES(13333333, 'Ringo', 'Star', 'rstar@gwmail.gwu.edu', 'HUec6nMmu9', 'Waiting for Review');
+INSERT INTO `Applications` VALUES(489649864, 'Ultimate', 13333333, 'MS', 'BS', 'Comp Sci', 'BU', '2.5', 'BS', '', '', '', '2013-04-17 16:29:05', 'worked for google', 'Fall 2013', '', 'Notviewed', '2010-04-24', NULL, NULL, '908-638-8466', '782-85-9631');
+INSERT INTO `Transcript` VAlues(489649864, 13333333, 2.5, 175, 25, 6, 147, 35);
+INSERT INTO `Recommendation` VALUES(163, 13333333, 'Yourk Appendi', 'Professor', 'yappendi@this.not.gwu', '', 'Professor for Algorithms', NULL);
+INSERT INTO `Recommendation` VALUES(164, 13333333, 'Jack Star', 'Father', 'jstar@this.not.gwu', '', 'He raised him to be the man he is', NULL);
+INSERT INTO `Recommendation` VALUES(165, 13333333, 'Robert Rodriguez', 'Manager', 'cevans@this.not.gwu', '', 'My boss and role model at google', NULL);
+
+INSERT INTO `Student` VALUES(14444444, 'Paul', 'McCartney', 'pmccartney@gwmail.gwu.edu', 'k38svO1hAy', 'Enrolled');
+INSERT INTO `Applications` VALUES(489649865, 'Ultimate', 14444444, 'MS', 'BS', 'Economics', 'BU', '3.65', 'BS', '', '', '', '2013-04-17 16:29:05', 'Worked at a consultant company', 'Fall 2013', '', 'viewed', '2010-04-24', 'looks good', 'Admit without Aid', '908-638-7821', '782-14-9631');
+INSERT INTO `Transcript` VAlues(489649865, 14444444, 3.65, 125, 14, 2, 142, 13);
+INSERT INTO `Recommendation` VALUES(166, 14444444, 'Yourk Appendi', 'Professor', 'yappendi@this.not.gwu', 'Very good student', 'Professor for Algorithms', '5');
+INSERT INTO `Recommendation` VALUES(167, 14444444, 'Jack Star', 'Father', 'jstar@this.not.gwu', 'Love this boy', 'He raised him to be the man he is', '3');
+INSERT INTO `Recommendation` VALUES(168, 14444444, 'Robert Rodriguez', 'Manager', 'cevans@this.not.gwu', 'Great coder', 'My boss and role model at google', '7');
+
+INSERT INTO `Student` VALUES(15555555, 'George', 'Harrison', 'gharrison@gwmail.gwu.edu', '2X3987mP7Y', 'Enrolled');
+INSERT INTO `Applications` VALUES(489649866, 'Asian Culture Society', 15555555, 'MS', 'BS', 'Japanese', 'BU', '3.87', 'BS', '', '', '', '2013-04-17 16:29:05', 'Worked at the Japanese Embassy', 'Fall 2013', '', 'viewed', '2010-04-24', 'really interesting applicant', 'Admit with Aid', '908-785-6315', '782-81-3244');
+INSERT INTO `Transcript` VAlues(489649866, 15555555, 3.87, 124, 17, 1, 125, 18);
+INSERT INTO `Recommendation` VALUES(169, 15555555, 'Yourk Appendi', 'Professor', 'yappendi@this.not.gwu', 'Great student that I even learned from', 'Professor for Japanese', '8');
+INSERT INTO `Recommendation` VALUES(170, 15555555, 'Jack Star', 'Father', 'jstar@this.not.gwu', 'He is scum', 'He raised him to be the man he is', '3');
+INSERT INTO `Recommendation` VALUES(171, 15555555, 'Robert Rodriguez', 'Manager', 'cevans@this.not.gwu', 'Helped diffuse numerous issues that our deparment ran into', 'My boss and role model at google', '9');
+
+INSERT INTO `app_to_reg` VALUES(14444444, 'Paul', 'McCartney', 'pmccartney@gwmail.gwu.edu', 'k38svO1hAy', 'MS', '908-638-7821', '782-14-9631');
+INSERT INTO `app_to_reg` VALUES(15555555, 'George', 'Harrison', 'gharrison@gwmail.gwu.edu', '2X3987mP7Y', 'MS', '908-785-6315', '782-81-3244');
--- Apps_original/list.php
+++ Apps_original/list.php
@@ -15,22 +15,27 @@ session_start();
 					if($_GET["search"]){
 						?>
 						<table border="1" style="width:600px; margin:0 17px 0 0">
-						<td><a>Click To REVIEW</a></td>
-						<td><a>Appcationid</a></td>
-						<td><a>Degree Seeking</a></td>
-						<td><a>GPA</a></td>
-						<td><a>Desired Semester</a></td>
-						<td><a>Application Submission Time</a></td>
-						<td><a>Application status</a></td>
+						<td>Click To REVIEW</td>
+						<td>Appcationid</td>
+						<td>Applicant Name</td>
+						<td>Degree Seeking</td>
+						<td>GPA</td>
+						<td>Desired Semester</td>
+						<td>Application Submission Time</td>
+						<td>Application status</td>
 						<?php
+						$name = preg_split('/\s/', $_GET["search"]);
+						$first = $name[0];
+						$last = $name[1];
 						$search_term=$_GET["search"];
 						require_once "config.php";
 						mysql_query("SET NAMES 'utf8'");
-						$query=$db->query("select * from Applications WHERE applicationid='$search_term' order by 'subdate'");
+						$query=$db->query("select * from Applications, Student WHERE Student.studentid=Applications.studentid AND (applicationid='$search_term' OR firstname='$first' OR lastname='$last' OR lastname='$first')  order by 'subdate'");
 						while($r=$db->fetch_array($query)){
 							echo "<tr>";
 							echo "<td><a href='view.php?id=".$r["applicationid"]."'>REVIEW</a></td>";
 							echo "<td>".$r["applicationid"]."</td>";
+							echo "<td>".$r["firstname"]." ".$r["lastname"]."</td>";
 							echo "<td>".$r["dsought"]."</td>";
 							echo "<td>".$r["pgpa"]."</td>";
 							echo "<td>".$r["desdate"]."</td>";
